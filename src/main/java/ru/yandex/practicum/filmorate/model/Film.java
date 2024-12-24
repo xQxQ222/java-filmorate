ba515470ab5b0d1e1932fefa2df2e26cc5128d1d
Ivan Korolkov <nerounxqxq@gmail.com>
2024-12-24 06:05:17 +0400

    Убраны неиспользованные импорты в классе Film

--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -1,14 +1,10 @@
 package ru.yandex.practicum.filmorate.model;
 
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Positive;
-import jakarta.validation.constraints.Size;
 import lombok.*;
-import lombok.extern.slf4j.Slf4j;
 
 import java.time.Duration;
 import java.time.LocalDate;
-import java.util.Date;
 
 @Getter
 @Setter
