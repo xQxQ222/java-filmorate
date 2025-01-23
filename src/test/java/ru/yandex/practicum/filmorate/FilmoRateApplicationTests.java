996e7ad412f848b1aa41a8fb97ed653f3b93d895
Ivan Korolkov <nerounxqxq@gmail.com>
2025-01-24 03:25:38 +0400

    Исправление код стайла

--- src/test/java/ru/yandex/practicum/filmorate/FilmoRateApplicationTests.java
+++ src/test/java/ru/yandex/practicum/filmorate/FilmoRateApplicationTests.java
@@ -5,7 +5,6 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.jdbc.JdbcTest;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.context.annotation.Import;
 import ru.yandex.practicum.filmorate.mapper.FilmMapper;
 import ru.yandex.practicum.filmorate.mapper.GenreMapper;
